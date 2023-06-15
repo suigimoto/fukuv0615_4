bbe373555a1d8fa96f7e3862d621a0826938e5bc
Sugimoto <sugimotodat@gmail.com>
2023-06-15 13:55:10 +0900

    たぶん完成

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,36 @@ namespace fukuv0615_4
 {
     public partial class Form1 : Form
     {
+        int ans = rand.Next(0, 100);
+        static Random rand = new Random();
+        int count = 0;
+        int guess;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show($"{count}回目の挑戦");
+
+            guess = int.Parse($"{textBox1.Text}");
+            if (ans == guess)
+            {
+                MessageBox.Show("Bingo!!");
+                count = 0;
+                ans = rand.Next(0, 5);
+
+            }
+            else if (ans > guess)
+            {
+                MessageBox.Show("Greater");
+            }
+            else
+            {
+                MessageBox.Show("Less");
+            }
         }
     }
 }
